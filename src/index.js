324860bc99334f582a002a849ddc8907b5a59f7c
Mona Lisa <mona@lisa.io>
2018-03-06 15:41:45 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
 const getUserName = () => {
-   var getName = readlineSync.question('Hello there, little monkey! Wanna become a human? I can help you! What is your name? Put your name here: ');
-console.log('Nice to meet you, ' + getName + '!');
+  const getName = readlineSync.question('Hello there, little monkey! Wanna become a human? I can help you! What is your name? Put your name here: ');
+  console.log(`Nice to meet you, ${getName} !`);
 };
 
 export default getUserName;
-
