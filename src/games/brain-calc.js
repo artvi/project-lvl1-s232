fd9c5158adc75651a9952c15f114631fa3b76787
Mona Lisa <mona@lisa.io>
2018-03-08 18:21:11 +0300

    removed eval

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,4 +1,4 @@
-import { cons } from 'hexlet-pairs';
+import { cons, car, cdr } from 'hexlet-pairs';
 import gameBody from '..';
 
 const currentTask = 'What is the result of the expression?';
@@ -13,17 +13,33 @@ const randomExpression = () => {
     }
     return '*';
   };
-  const a = Math.floor(Math.random() * 100);
-  const oper = randOperator(Math.floor(Math.random() * 100));
-  const b = Math.floor(Math.random() * 100);
-  const expression = `${a} ${oper} ${b}`;
 
-  return expression;
+  const resultOfExpression = () => {
+    const a = Math.floor(Math.random() * 50);
+    const oper = randOperator(Math.floor(Math.random() * 100));
+    const b = Math.floor(Math.random() * 50);
+    const expression = `${a} ${oper} ${b}`;
+    let result = 0;
+
+    if (oper === '+') {
+      result = a + b;
+    }
+    if (oper === '-') {
+      result = a - b;
+    }
+    if (oper === '*') {
+      result = a * b;
+    }
+
+    return cons(expression, result);
+  };
+  return resultOfExpression();
 };
 
 const game = () => {
-  const question = randomExpression();
-  const correctAnswer = eval(question);
+  const task = randomExpression();
+  const question = car(task);
+  const correctAnswer = cdr(task);
   return cons(question, correctAnswer);
 };
 export default () => gameBody(currentTask, game);
